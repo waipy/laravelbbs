c94eb4c97cb8e0d9c2a13573bf13982b042e4cd6
liweisheng <liweisheng1234@qq.com>
2017-11-27 16:14:17 +0800

    User can create topic

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -10,16 +10,14 @@ class TopicRequest extends Request
         {
             // CREATE
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
             // UPDATE
             case 'PUT':
             case 'PATCH':
             {
                 return [
+                    'title' => 'required|min:2',
+                    'body' => 'required|min:3',
+                    'category_id' => 'required|numeric',
                     // UPDATE ROLES
                 ];
             }
@@ -36,6 +34,8 @@ class TopicRequest extends Request
     {
         return [
             // Validation messages
+            'title.min'=> '标题必须至少两个字符',
+            'body.min'=>'文章内容必须至少三个字符',
         ];
     }
 }
