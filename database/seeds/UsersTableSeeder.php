c440b6e0c4dd96c1ffd4977f9772913f02fc1cca
liweisheng <liweisheng1234@qq.com>
2018-09-19 17:59:09 +0800

    Integrate laravel-permission package

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -41,5 +41,10 @@ class UsersTableSeeder extends Seeder
         $user->email = 'liweisheng1234@qq.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
+
+        $user->assignRole('Founder');
+
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
